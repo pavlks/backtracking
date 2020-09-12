8734da1259bd100cbc16b4806dbdfd7d672b0f23
Pavel Sazonov <sazonov.ps@pm.me>
2020-09-12 17:10:05 -0500

    done

--- main.py
+++ main.py
@@ -2,18 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+from pprint import pprint
 
 
 sudoku = (
-    [0, 0, 0, 1, 0, 5, 0, 6, 8],
-    [0, 0, 0, 0, 0, 0, 7, 0, 1],
-    [9, 0, 1, 0, 0, 0, 0, 3, 0],
-    [0, 0, 7, 0, 2, 6, 0, 0, 0],
-    [5, 0, 0, 0, 0, 0, 0, 0, 3],
-    [0, 0, 0, 8, 7, 0, 4, 0, 0],
-    [0, 3, 0, 0, 0, 0, 8, 0, 5],
-    [1, 0, 5, 0, 0, 0, 0, 0, 0],
-    [7, 9, 0, 4, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 2, 0, 0],
+    [0, 0, 0, 0, 0, 0, 3, 4, 0],
+    [0, 0, 0, 0, 0, 0, 0, 5, 6],
+    [0, 0, 0, 0, 0, 0, 0, 0, 7],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [7, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 4, 8, 0, 0, 0, 0, 0, 0],
+    [0, 0, 6, 3, 0, 0, 0, 0, 0],
 )
 
 
@@ -34,10 +36,10 @@ def next_empty(row=0, col=0):
             if sudoku[r][c] == 0:
                 return r, c
             else:
-                'done'
+                return False
 
 
-def possible_numbers(table, row=0, col=0):
+def possible_numbers(table, row, col):
     numbers_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
     col_nums = list()
 
@@ -54,29 +56,28 @@ def possible_numbers(table, row=0, col=0):
         for j in range(3):
             square_nums.append(table[start_row + i][start_col + j])
 
-    forbidden_numbers_set = set(table[row]).union(set(col_nums)).union(square_nums)
+    # не будем учитывать цифру, стоящую на этой позиции
+    table_row = list(table[row])
+    table_row.remove(table[row][col])
+
+    forbidden_numbers_set = set(table_row).union(set(col_nums)).union(square_nums)
     return sorted(list(numbers_set.difference(forbidden_numbers_set)))
 
 
-def try_number(position, possible_numbers):
+def solve(sudoku_table):
+    empties = get_all_empty(sudoku_table)
+    if not empties:
+        pprint(sudoku_table)
+        exit()
+    ne = empties[0]
+    pn = possible_numbers(sudoku_table, ne[0], ne[1])
+    while pn:
+        for num in pn:
+            sudoku_table[ne[0]][ne[1]] = num
+            solve(sudoku_table)
+        sudoku_table[ne[0]][ne[1]] = 0
+        break
+    return sudoku_table
 
 
-
-def solve(table, depth=0):
-    empty_positions = get_all_empty(table)
-    solved_table = table
-
-    while depth != len(empty_positions):
-        for e in empty_positions:
-
-            
-
-    possib_solut = possible_numbers(solved_table, empty_positions[depth][0], empty_positions[depth][1])
-    if possib_solut:
-        for num in possib_solut:
-            solved_table[empty_positions[0][0]][empty_positions[0][1]] = num
-            attempt(solved_table, depth + 1)
-    return solved_table
-
-
-print(attempt(sudoku))
+solve(sudoku)
